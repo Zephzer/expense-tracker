ca29b1cd5abcfbbf3b28fe303ed5f852b01233a2
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-19 19:21:55 +0800

    feat: add new page

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const exhbs =require('express-handlebars')
 const routes = require('./routes/index')
+const bodyParser = require('body-parser')
 const port = 3000
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended:true }))
 
 app.use(routes)
 
--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/new', (req, res) => {
     return res.render('new')
 })
 
+router.post('/', (req, res) => {
+    console.log(req.body)
+    // console.log(selectValue)
+})
 
 module.exports = router
