1d72701db2a32d685791ae9b08e9baf55d9f2641
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-19 17:18:43 +0800

    feat:add express-handlebars and index page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const exhbs =require('express-handlebars')
 const port = 3000
 
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-    res.send('OK')
+    res.render('index')
 })
 
 app.listen(port, () => {
