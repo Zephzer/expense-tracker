c84292e645e2f8a2163fcba7dbab343d98ecf8b9
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-20 17:20:45 +0800

    feat: add register function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const exhbs =require('express-handlebars')
 const routes = require('./routes/index')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -14,8 +16,23 @@ require('./config/mongoose')
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended:true }))
 app.use(methodOverride('_method'))
+
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.warning_msg = req.flash('warning_msg')
+    next()
+})
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,67 @@
 const express = require('express')
+const User = require('../../models/user')
+const passport = require('passport')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
+// link to login page
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
+// login function
+
+
+// link to register page
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+// register function
+router.post('/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與密碼確認不相符' })
+    }
+    if (errors.length) {
+        return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+        })
+    }
+
+    User.findOne({ email }).then(user => {
+        if (user) {
+            errors.push({ message: '這個 Email 已經被註冊過了。' })
+            return res.render('register', {
+                errors,
+                name,
+                email,
+                password,
+                confirmPassword
+            })
+        }
+        return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(password, salt))
+            .then(hash => {
+                User.create({
+                    name,
+                    email,
+                    password: hash
+                })
+            })
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+    })
+        .catch(err => console.log(err))
 })
 
 module.exports = router
