101b576d2b1195df60276541a98b999788091258
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-20 18:38:57 +0800

    feat: add facebook login

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ const router = express.Router()
 const record = require('./modules/record')
 const home = require('./modules/home')
 const users = require('./modules/users')
-
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 
 router.use('/record', authenticator ,record)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator ,home)
 
 module.exports = router
