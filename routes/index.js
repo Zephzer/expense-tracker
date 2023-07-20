fff68af54364fbd45d30637a4143e4b01e720443
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-20 16:55:45 +0800

    feat: add login and register page

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 
 const record = require('./modules/record')
 const home = require('./modules/home')
+const users = require('./modules/users')
 
 router.use('/record', record)
+router.use('/users', users)
 router.use('/', home)
 
 module.exports = router
