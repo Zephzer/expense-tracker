9e2b07a409343e4d58b7c38d6e927f189e85f0fb
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-20 17:40:26 +0800

    feat: add user authentication in record and home routes

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 const record = require('./modules/record')
 const home = require('./modules/home')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/record', record)
+
+router.use('/record', authenticator ,record)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator ,home)
 
 module.exports = router
