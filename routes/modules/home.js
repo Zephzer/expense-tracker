a7ebd4c98c5d6ebf46eb7d65cc08d9ad49bfffd6
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-21 14:03:11 +0800

    feat: add search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -23,4 +23,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/search', async (req, res) => {
+    try {
+        const userId = req.user._id
+        const keyword = req.query.keyword
+        let totalAmount = 0
+        const records = await Record.find({ userId, category: keyword }).lean().sort({ date: 'desc' })
+        await Promise.all(
+            records.map(async (record) => {
+                const category = await Category.find({ categoryName: record.category }).lean()
+                record.image = category[0].image
+                const amount = record.amount
+                totalAmount += amount    
+            })
+        )
+        res.render('index', { records, totalAmount })
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
 module.exports = router
